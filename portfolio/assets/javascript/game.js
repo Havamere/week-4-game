97c2e960d94cc9f3d027d4b97d6d089cf5770a0c
Kyle Rinehart <Kyle Rinehart>
2016-04-24 21:56:05 -0400

    formed JS to create character divs, but image is not showing up

--- portfolio/assets/javascript/game.js
+++ portfolio/assets/javascript/game.js
@@ -1,8 +1,17 @@
 var characters = [
-	{Name: "Han Solo"; "Health Points": "120"; "Attack Power": 6; "Counter Attack Power": 12; source: "assets/images/han-solo.jpg"; background: "assets/images/mos_eisley.jpg"}
-	{Name: "Darth Vader"; "Health Points": "150"; "Attack Power": 8; "Counter Attack Power": 16; source: "assets/images/darth-vader.jpg"; background: "assets/images/death-star1.jpg"}
-	{Name: "Yoda"; "Health Points": "180"; "Attack Power": 10; "Counter Attack Power": 20; source: "assets/images/yoda.jpg"; background: "assets/images/dagoba.jpeg"}
-	{Name: "Boba Fett"; "Health Points": "220"; "Attack Power": 12; "Counter Attack Power": 25; source: "assets/images/boba-fett.jpg"; background: "assets/images/cloud-city.png"}
+	{Name: "Han Solo", HealthPoints: "120", AttackPower: 6, CounterAttackPower: 12, source: "assets/images/han-solo.jpg", background: "assets/images/mos_eisley.jpg"},
+	{Name: "Darth Vader", HealthPoints: "150", AttackPower: 8, CounterAttackPower: 16, source: "assets/images/darth-vader.jpg", background: "assets/images/death-star1.jpg"},
+	{Name: "Yoda", HealthPoints: "180", AttackPower: 10, CounterAttackPower: 20, source: "assets/images/yoda.jpg", background: "assets/images/dagoba.jpeg"},
+	{Name: "Boba Fett", HealthPoints: "220", AttackPower: 12, CounterAttackPower: 25, source: "assets/images/boba-fett.jpg", background: "assets/images/cloud-city.png"}
 ];
 
-$("#character-1").add()
+for (var i=0; i<characters.length; i++){
+	$("#"+i).attr({
+		Name: characters[i].Name,
+		HealthPoints: characters[i].HealthPoints,
+		AttackPower: characters[i].AttackPower,
+		CounterAttackPower: characters[i].CounterAttackPower,
+		src: characters[i].source,
+		background: characters[i].background,
+	})
+}
