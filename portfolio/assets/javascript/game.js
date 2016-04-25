3454e2828cdc41246d1fe73ad9719f58e3a58455
Kyle Rinehart <Kyle Rinehart>
2016-04-24 20:13:44 -0400

    scaffold of html set up for game, characters set up in JS.

---
+++ portfolio/assets/javascript/game.js
@@ -0,0 +1,8 @@
+var characters = [
+	{Name: "Han Solo"; "Health Points": "120"; "Attack Power": 6; "Counter Attack Power": 12; source: "assets/images/han-solo.jpg"; background: "assets/images/mos_eisley.jpg"}
+	{Name: "Darth Vader"; "Health Points": "150"; "Attack Power": 8; "Counter Attack Power": 16; source: "assets/images/darth-vader.jpg"; background: "assets/images/death-star1.jpg"}
+	{Name: "Yoda"; "Health Points": "180"; "Attack Power": 10; "Counter Attack Power": 20; source: "assets/images/yoda.jpg"; background: "assets/images/dagoba.jpeg"}
+	{Name: "Boba Fett"; "Health Points": "220"; "Attack Power": 12; "Counter Attack Power": 25; source: "assets/images/boba-fett.jpg"; background: "assets/images/cloud-city.png"}
+];
+
+$("#character-1").add()
